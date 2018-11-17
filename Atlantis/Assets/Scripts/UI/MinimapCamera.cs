35c4a7766f457f833e8005bc6b5a6b03a37004fa
martinsimov9a19 <martinsimov519@gmail.com>
2018-11-17 13:46:08 +0200

    Added Minimap System and fixed character bugs

---
+++ Atlantis/Assets/Scripts/UI/MinimapCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+
+    private GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 newPos = player.transform.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+	}
+}
