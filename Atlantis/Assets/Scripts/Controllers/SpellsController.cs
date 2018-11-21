1bdaa395a9a87b652a41280f8a0336ae4360ceae
martinsimov9a19 <martinsimov519@gmail.com>
2018-11-21 22:34:25 +0200

    Added Warrior First Spell

---
+++ Atlantis/Assets/Scripts/Controllers/SpellsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellsController : MonoBehaviour {
+
+    private float damage;
+    private bool spellOneCooldown = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("1") && spellOneCooldown == false) {
+            gameObject.GetComponent<PlayerStats>().spellOneActive = true;
+            damage = gameObject.GetComponent<PlayerStats>().damage;
+            gameObject.GetComponent<PlayerStats>().damage += gameObject.GetComponent<PlayerStats>().damage * 0.5f;
+            spellOneCooldown = true;
+            StartCoroutine(SpellOneCooldown());
+
+        }
+    }
+
+    IEnumerator SpellOneCooldown() {
+        yield return new WaitForSeconds(3f);
+        gameObject.GetComponent<PlayerStats>().spellOneActive = false;
+        gameObject.GetComponent<PlayerStats>().damage = damage;
+        yield return new WaitForSeconds(3f);
+        spellOneCooldown = false;
+
+    }
+
+}
--- Atlantis/Assets/Scripts/Stats/PlayerStats.cs
+++ Atlantis/Assets/Scripts/Stats/PlayerStats.cs
@@ -9,11 +9,17 @@ public class PlayerStats : MonoBehaviour {
     public float cooldown = 1f;
     public float level = 1f;
     public float experience = 0f;
+
+    public float spell1Cooldown = 5f,spell2Cooldown = 5f,spell3Cooldown = 5f,spell4Cooldown = 5f,spell5Cooldown=5f;
+
+
     public int storageCapacity = 20;
     public float healthRegen = 2f;
     public bool inCombat = false;
     private static float levelExp;
     private float healthRegenCooldown = 1f, timer;
+
+    public bool spellOneActive = false;
 
     void Start()
     {
@@ -44,7 +50,7 @@ public class PlayerStats : MonoBehaviour {
     }
 
     public void DealDamage(float damageAmount) {
-        if (Health > 0)
+        if (Health > 0 && spellOneActive == false)
         {
             Health = Health - damageAmount;
         }
